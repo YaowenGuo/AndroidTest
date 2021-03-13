098d937d4ed5f959f6c71fc130f88bae11c2ecb9
Albert <guobool@163.com>
2021-03-13 22:43:35 +0800

    Test Rxjava.

--- lib/TestRxJava/src/main/java/tech/yaowen/testrxjava/TestComsumer.kt
+++ lib/TestRxJava/src/main/java/tech/yaowen/testrxjava/TestComsumer.kt
@@ -1,13 +1,35 @@
 package tech.yaowen.testrxjava
 
+import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.core.SingleObserver
 import io.reactivex.rxjava3.functions.Consumer
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 fun main() {
     Single.just(0)
         .doOnSubscribe(Consumer {
+            println("Single: ------")
 
         })
         .subscribe()
+    println("time: ------")
+
+    Observable.intervalRange(0, 30, 0,1, TimeUnit.SECONDS)
+        .doOnSubscribe(Consumer {
+            println("Observable: ------")
+
+        })
+        .subscribeOn(Schedulers.newThread())
+        .subscribe({ time ->
+            println("time: $time")
+        }, { exception ->
+
+        }, {
+            println("complete")
+        })
+    println("time: end")
+
+    Thread.sleep(600000)
 }
