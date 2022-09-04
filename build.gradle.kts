1c90ef6804b929a5059a0b18e738c0061164b480
Yaowen <guobool@163.com>
2022-09-04 10:35:15 +0800

    Migrite gradle to kts.

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("com.android.application") version "7.2.2" apply false
+    id("com.android.library") version "7.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
+    id("org.mozilla.rust-android-gradle") version "0.9.0" apply false
+    id("androidx.navigation.safeargs") version "2.4.1" apply false
+}
+
+val clean by tasks.creating(Delete::class) {
+    delete(rootProject.buildDir)
+}
